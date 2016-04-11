0fdf84a07208424f9df576d19ab61a63a1642b28
Rose <rcampau@icloud.com>
2016-04-11 17:01:01 -0400

    initial commit

---
+++ CampauPortfolio/script.js
@@ -0,0 +1,6 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='home.html'>Home</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"<li><a href='contact.html'>Contact</a></li>" +
+"</ul>";
